7d1f837b847751f451279c4ef7cad012f9693249
Dimitar Zahariev <dimitar@zahariev.pro>
2021-11-18 17:14:31 +0200

    Update index.php

--- code/index.php
+++ code/index.php
@@ -4,7 +4,7 @@
 <body>
 <form action="save.php" method="post">
   <label>Enter your note here:</label><br />
-  <input type="text" name="note" id="note"><br />
+  <input type="text" name="note" id="note"><br /><br />
   <input type="submit" value="Save">
 </form>
 <?php
@@ -19,10 +19,15 @@
       fclose($f);
     }
     else
-      print "No data to show.";
+      print "No data to show.<br />\n";
   }
   else
-    print "No data to show.";
+    print "No data to show.<br />\n";
+
+  print "<br /><hr /><br />\n";
+
+  print "<small><i>Working on <b>".gethostname()."</b></i></small>\n";
+
 ?>
 </body>
 </html>
